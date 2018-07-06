673ecc0a907f4f7cdb0ded2a2a2e05e8e1e1e027
SelimAbidin <ysabidin@gmail.com>
2018-07-06 15:27:56 +0300

    inital commit, uniq methods including specs

---
+++ specs/uniq.spec.js
@@ -0,0 +1,146 @@
+import {uniq, uniqBy,uniqByProps} from "../src/uniq";
+// const {uniqueBy} = require("../src/uniqueBy");
+const deepFreeze = require("deepfreeze");
+
+describe("uniq testing", () => {
+
+    it("[1,2,3,3] result should be [1,2,3]", () => {
+        const array = [1,2,3,3];
+        const uniqs = uniq(array)
+        expect(uniqs).toEqual([1,2,3])
+    })
+
+    it("[3,1,2,3] result should be [3,1,2]", () => {
+        const array = [3,1,2,3];
+        const uniqs = uniq(array)
+        expect(uniqs).toEqual([3,1,2])
+    })
+
+    it("should return a new array", () => {
+        const array = deepFreeze([1,2,3,3]);
+        const uniqs = uniq(array)
+        expect(uniqs).toEqual([1,2,3])
+    })
+})
+
+
+describe("uniqBy testing", () => {
+
+    it("should be objects with duplicate x values removed", () => {
+        const array = [
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+        ];
+        const uniqs = uniqBy(array, "x")
+        expect(uniqs).toEqual([{x:1, y:1},{x:2, y:1}])
+    })
+
+
+    it("should be objects with duplicate y values removed", () => {
+        const array = [
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+        ];
+        const uniqs = uniqBy(array, "y")
+        expect(uniqs).toEqual([{x:1, y:1}])
+    })
+
+    it("should be objects with duplicate y values removed", () => {
+        const array = deepFreeze([
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+        ]);
+        const uniqs = uniqBy(array, "y")
+        expect(uniqs).toEqual([{x:1, y:1}])
+    })
+
+})
+
+describe("uniqByProps testing", () => {
+
+    it("should be object with duplicate properties ([x,y]) removed", () => {
+        const array = [
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+        ];
+        const uniqs = uniqByProps(array, ["x", "y"])
+        expect(uniqs).toEqual([{x:1, y:1},{x:2, y:1}])
+    })
+
+    it("should be objects with duplicate properties ([x,y]) removed", () => {
+        const array = [
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+        ];
+        const uniqs = uniqByProps(array, ["x", "y"])
+        expect(uniqs).toEqual([{x:1, y:1},{x:2, y:1}])
+    })
+
+
+    it("should be objects with duplicate properties ([x,y]) removed", () => {
+        const array = [
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:2, y:1},
+            {x:2, y:1},
+            {x:2, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+            {x:1, y:1},
+        ];
+        const uniqs = uniqByProps(array, ["x", "y"])
+        expect(uniqs).toEqual([{x:1, y:1},{x:2, y:1}])
+    })
+
+    it("should be mutate array passed as an argument", () => {
+        const array = deepFreeze([
+            {x:1, y:1},
+            {x:2, y:1},
+            {x:1, y:1},
+        ]);
+
+        const uniqs = uniqByProps(array, ["x", "y"])
+        expect(uniqs).toEqual([{x:1, y:1},{x:2, y:1}])
+    })
+
+    it("should be object with duplicate properties ([x,y]) removed and keep duplicate (z) params ", () => {
+        const array = [
+            {x:1, y:1, z:1},
+            {x:2, y:1, z:2},
+            {x:1, y:1, z:3},
+        ];
+        const uniqs = uniqByProps(array, ["x", "y"])
+        expect(uniqs).toEqual([{x:1, y:1, z:1},{x:2, y:1,z:2}])
+    })
+
+})
---
+++ src/uniq.js
@@ -0,0 +1,70 @@
+
+
+/**
+ * creates a new array from the input array and remove second repeating elements.
+ * [3,1,2,3] >> uniq >> [3,1,2] 
+ * @param {Array} array 
+ * @param {any} array 
+ * @returns {Array}
+ */
+export function uniq(array) {
+    return Array.from(new Set(array))
+}
+
+/**
+ * 
+ * @param {Array} array 
+ * @param {any} property
+ * @param {any} out (optional)
+ * @returns {Array}
+ */
+export function uniqBy(array, property, out) {
+    
+    let seenValue = {}
+    let temp
+    return array.reduce((acc, cur) => {
+
+        temp = cur[property]
+        if(!seenValue[temp]) {
+            seenValue[temp] = true
+            acc.push(cur);
+        }
+        
+        return acc;
+    }, out !== undefined ? out : [])
+}
+
+/**
+ * 
+ * @param {Array} array 
+ * @param {string[]} property
+ * @returns {Array}
+ */
+export function uniqByProps(array, properties) {
+
+    let i,j,counter,hasAnother,element,element2
+
+    let copyArray = array.concat()
+    for (i = 0; i < copyArray.length; i++) {
+        element = copyArray[i];
+        
+        hasAnother = false
+        for (j = i + 1; j < copyArray.length; j++) {
+            element2 = copyArray[j];
+            counter = 0
+            properties.forEach(p => {
+                            if(element[p] === element2[p]) {
+                                counter++;
+                            }
+                        })
+
+            if(counter === properties.length) {
+                let sliced = copyArray.splice(j, 1)
+                j--
+            }
+        }
+       
+    }
+    
+    return copyArray
+}
