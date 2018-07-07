d4407f10c073e28c169bd4732230809ff6d65637
SelimAbidin <ysabidin@gmail.com>
2018-07-07 22:39:09 +0300

    eslint added

--- reverse.js
+++ reverse.js
@@ -1,11 +1,9 @@
-
-
 /**
  * reverse creates a new array and reverse it before return.
- * If you want to do mutate the array, you will probably be ok with Array.prototype.reverse method too. 
+ * If you want to do mutate the array, you will probably be ok with Array.prototype.reverse method too.
  * @param {array<any>} array
  * @return {array}
  */
-export function reverse(array) {
+export function reverse (array) {
     return array.concat().reverse()
 }
--- uniq.js
+++ uniq.js
@@ -1,71 +1,65 @@
 
-
 /**
  * creates a new array from the input array and remove second repeating elements.
- * [3,1,2,3] >> uniq >> [3,1,2] 
- * @param {Array} array 
- * @param {any} array 
+ * [3,1,2,3] >> uniq >> [3,1,2]
+ * @param {Array} array
+ * @param {any} array
  * @returns {Array}
  */
-function uniq(array) {
+function uniq (array) {
     return Array.from(new Set(array))
 }
 
 /**
- * 
- * @param {Array} array 
+ *
+ * @param {Array} array
  * @param {any} property
  * @param {any} out (optional)
  * @returns {Array}
  */
-function uniqBy(array, property, out) {
-    
+function uniqBy (array, property, out) {
     let seenValue = {}
     let temp
     return array.reduce((acc, cur) => {
-
         temp = cur[property]
-        if(!seenValue[temp]) {
+        if (!seenValue[temp]) {
             seenValue[temp] = true
             acc[acc.length] = cur
         }
-        
+
         return acc
     }, out !== undefined ? out : [])
 }
 
 /**
- * 
- * @param {Array} array 
+ *
+ * @param {Array} array
  * @param {string[]} property
  * @returns {Array}
  */
-function uniqByProps(array, properties) {
-
-    let i,j,counter,hasAnother,element,element2
+function uniqByProps (array, properties) {
+    let i, j, counter, element, element2
 
     let copyArray = array.concat()
     for (i = 0; i < copyArray.length; i++) {
         element = copyArray[i]
-        
-        hasAnother = false
+
         for (j = i + 1; j < copyArray.length; j++) {
             element2 = copyArray[j]
             counter = 0
             properties.forEach(p => {
-                            if(element[p] === element2[p]) {
-                                counter++
-                            }
-                        })
+                if (element[p] === element2[p]) {
+                    counter++
+                }
+            })
 
-            if(counter === properties.length) {
+            if (counter === properties.length) {
                 copyArray.splice(j, 1)
                 j--
             }
         }
-       
     }
-    
+
     return copyArray
 }
 
