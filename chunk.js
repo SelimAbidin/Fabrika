043051b7649374b4e773bcffe8f38fd8be2f3ecd
Y.Selim Abidin <ysabidin@gmail.com>
2019-11-14 14:43:17 +0300

    chunk mutation fix

--- chunk.js
+++ chunk.js
@@ -1,6 +1,8 @@
 
 function chunk (array, count) {
-    return Array.from({ length: array.length / count }).map(() => array.splice(0, count))
+    return Array.from({ length: array.length / count })
+        .map((_element, index) => array
+            .slice(index * count, (index + 1) * count))
 }
 
 module.exports = {
--- specs/chunk.spec.js
+++ specs/chunk.spec.js
@@ -1,10 +1,18 @@
 import { chunk } from '../chunk'
 
-// const deepFreeze = require('deepfreeze')
+const deepFreeze = require('deepfreeze')
 
 describe('chunk testing', () => {
     it('[1,2,3,3] result should be [[1,2],[3,3]]', () => {
         const array = [1, 2, 3, 3]
+        let chunks = chunk(array, 2)
+        expect(chunks.length).toEqual(2)
+        expect(chunks[0]).toEqual([1, 2])
+        expect(chunks[1]).toEqual([3, 3])
+    })
+
+    it('freeze([1,2,3,3]) result should be [[1,2],[3,3]]', () => {
+        const array = deepFreeze([1, 2, 3, 3])
         let chunks = chunk(array, 2)
         expect(chunks.length).toEqual(2)
         expect(chunks[0]).toEqual([1, 2])
